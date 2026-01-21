0296e964187b399610bd7158f7c8ea8cbca25d32
Ada Alakbarova <ada.alakbarova@proton.me>
2026-01-21 21:52:06 +0100

    Add language localisation support

---
+++ i18n/en/cosmic_ext_applet_sysinfo.ftl
@@ -0,0 +1,1 @@
+include-swap-in-ram-toggle = Include swap in RAM
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod applet;
 pub mod config;
+pub(crate) mod i18n;
 
 fn main() -> cosmic::iced::Result {
     // Initialize logging
@@ -8,6 +9,9 @@ fn main() -> cosmic::iced::Result {
         .init();
 
     tracing::info!("Starting sysinfo applet");
+
+    let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
+    i18n::init(&requested_languages);
 
     applet::run()
 }
